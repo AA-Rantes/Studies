f0549499dd8067627707eaab692844bd76e7bcf0
AArantes <aarantes@DESKTOP-HLE2L4E.>
2025-02-23 15:31:37 +0000

    i have 0 ideas about C#, will watch some tutorials and come back later

---
+++ LEARNING C#/classes.cs
@@ -0,0 +1,24 @@
+using System
+
+static void Main(void)
+{
+	Console.WriteLine(Names);
+}
+
+class Names
+{
+		public static void Rato()
+		{
+			Console.WriteLine("Rato");
+		}
+
+		public static void Abelha()
+		{
+			Console.WriteLine("Abelha");
+		}
+
+		public static void Barata()
+		{
+			Console.WriteLine("Barata");
+		}
+}
