42ed1217e8d91e830fb037779fc42fb8336482d9
AArantes <arthurarantes274@gmail.com>
2025-03-08 14:09:55 +0000

    added executable file for PutcharWithHeader

--- C STUDIES/OTHER/EXERCISES/PutcharWithHeader/main.c
+++ C STUDIES/OTHER/EXERCISES/PutcharWithHeader/main.c
@@ -1,3 +1,4 @@
+
 #include    <unistd.h>
 #include    "ft_putchar_header.h"
 
@@ -5,4 +6,10 @@
 int main(void)
 {
 	ft_putchar('a');
+	ft_putchar('b');
+	ft_putchar('e');
+	ft_putchar('l');
+	ft_putchar('h');
+	ft_putchar('a');
+	ft_putchar('\n');
 }
